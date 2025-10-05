7262ac8fc18795f11a8deeacec33ce6184d2141d
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2025-10-05 20:13:12 +0300

    fix: commitlint config type

---
+++ commitlint.config.js
@@ -0,0 +1,26 @@
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "footer-max-line-length": [1, "always", 180],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "rfc",
+        "docs",
+        "style",
+        "improvement",
+        "enhancement",
+        "refactor",
+        "perf",
+        "test",
+        "chore",
+        "build",
+        "ci",
+        "revert",
+      ],
+    ],
+  },
+};
